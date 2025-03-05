be5c38eda1947a391eaf0985470f132e0769ee9c
Caio <oiaccf>
2025-03-05 15:55:32 -0300

    adicionando nomes dentro do array

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    let nome = document.querySelector('input').value;
+    if (nome == '') {
+        alert('Por favor, digite um nome válido.');
+        return;
+    }
+    amigos.push(nome);
+    console.log(amigos);
+}
